8b650fec76631aa72d3fc71ca6eca45fc4944514
YnotC <toekneechin99@gmail.com>
2018-10-20 14:09:54 -0400

    file

--- nearest_roads.py
+++ nearest_roads.py
@@ -20,11 +20,11 @@ def translation_generator(perimeter, location):
     w = perimeter/2 - h
     lat_conversion = 68.703 + abs(location['lat']/90) * 0.704
     lng_conversion = 69.172 * math.cos(math.radians(location['lat']))
-    lat = h/lat_conversion * -1 ^ randint(0,1)
-    lng = w/lng_conversion * -1 ^ randint(0,1)
+    lat = h/lat_conversion * -1 ** random.randint(0,1)
+    lng = w/lng_conversion * -1 ** random.randint(0,1)
     return [lat, lng]
 
-"""takes an origin point and a distance length and returns a dictionary of directions of points to tuples of coordinates"""
+"""takes an origin name string and a distance length and returns a dictionary of directions of points to tuples of coordinates"""
 def rectangle_generator(originAddress, distance):
     origin_dictionary = pointfinder(originAddress)
     translations = translation_generator(distance, origin_dictionary)
@@ -35,15 +35,16 @@ def rectangle_generator(originAddress, distance):
     return {'origin': origin, 'vertical': vertical, 'horizontal': horizontal, 'diagonal': diagonal}
 
 """takes a list of coordinate tuples, returns points formatted for nearest road API"""
-def points_formatter(coordinates):
+def points_formatter(rectangle):
     formattedPoints = ''
+    coordinates = rectangle.values()
     for coord in coordinates:
         formattedCoord = str(coord).replace(' ', '').replace('(','').replace(')','')
         formattedPoints += '|' + formattedCoord
-    return formattedPoints
+    return formattedPoints[1:]
 
 """takes a formatted string of points and returns a path through the points"""
 def generate_path(points):
     url = 'https://roads.googleapis.com/v1/snapToRoads?'
-    resp = requests.get(url, params = {'path': points})
+    resp = requests.get(url, params = {'path': points, 'key': 'AIzaSyAYO7T7rV7bUOer87rKnXLXXffZG_fh-LE', 'interpolate': 'true'})
     return list(resp)
